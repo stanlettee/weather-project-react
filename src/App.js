c2faeb36f0e2ff7e79cffe5f086c2ffa3326eca9
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2026-02-02 21:47:02 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import { Header } from "./components/Header"
-import { Footer } from "./components/Footer"
-import { Gallery } from "./components/Gallery"
-import { Hero } from "./components/Hero"
-import { Modal } from "./components/Modal"
-import { News } from "./components/News"
-import { Weather } from "./components/Weather"
+// import { Footer } from "./components/Footer"
+// import { Gallery } from "./components/Gallery"
+// import { Hero } from "./components/Hero"
+// import { Modal } from "./components/Modal"
+// import { News } from "./components/News"
+// import { Weather } from "./components/Weather"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      {/* <main>
+        <Hero />
+        <Weather />
+        <News />
+        <Gallery />
+      </main> 
+      <Footer />
+      <Modal /> */}
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,44 @@
 import styles from './styles/Header.module.css'
+import logoMobile from '../images/logo-mobile-1.webp'
+import logoTablet from '../images/logo-tablet-1.webp'
+import logoDesktop from '../images/logo-desktop-1.webp'
+import user from '../images/user.webp'
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+export const Header = () => {
+
+
+    return (
+        <header className={styles.header}>
+            <div className={`${styles.container} container`}>
+                <picture className={styles.headerImage}>
+                    <source media="(min-width: 1200px)" srcSet={logoDesktop} />
+                    <source media="(min-width: 768px)" srcSet={logoTablet} />
+                    <img src={logoMobile} alt="Description of image" />
+                </picture>
+                <ul className={styles.headerList}>
+                    <li className={styles.headerItem}>
+                        <a className={styles.headerLink} href=''>Who we are</a>
+                    </li>
+                    <li className={styles.headerItem}>
+                        <a className={styles.headerLink} href=''>Contacts</a>
+                    </li>
+                    <li className={styles.headerItem}>
+                        <a className={styles.headerLink} href=''>Menu</a>
+                    </li>
+                </ul>
+                <div className={styles.headerDiv}>
+                    <button type='button' className={styles.headerButton}>Sign Up</button>
+                    <img className={styles.headerAvatar} src={user} alt="user's avatar"/>
+                </div>
+                <div className={styles.headerMenu}>
+                    <button type='button' className={styles.headerMenuButton}>
+                        <a className={styles.headerMenuLink}>Menu</a>
+                        <MdOutlineKeyboardArrowRight className={styles.headerMenuIcon}/>
+                    </button>
+                    
+                </div>
+            </div>
+        </header>
+    )
+}
